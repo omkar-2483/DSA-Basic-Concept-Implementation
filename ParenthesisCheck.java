33ebe5ed8a37b7d665efc4b5339db98a366fda09
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2024-01-24 07:51:06 +0530

    Add files via upload

---
+++ ParenthesisCheck.java
@@ -0,0 +1,27 @@
+package DSAProblems;
+import java.util.*;
+
+public class ParenthesisCheck {
+	public static void checkParenthesis(String s) {
+		Stack<Character> st=new Stack<Character>();
+		for(char c: s.toCharArray()) {
+			if(c=='(') {
+				st.push(')');
+			}else if(!st.isEmpty()&& st.peek()==c) {
+				st.pop();
+			}else {
+				System.out.println("Input string is invalid parenthesis pattern");
+				return;
+			}
+		}
+		if(st.isEmpty()) {
+			System.out.println("Input string is valid parenthesis pattern");
+		}else {
+			System.out.println("Input string is invalid parenthesis pattern");
+		}
+	}
+	public static void main(String[] args) {
+		String str=new String("())()()");
+		checkParenthesis(str);
+	}
+}
