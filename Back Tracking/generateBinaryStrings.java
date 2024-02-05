5502ab0e4d3052c37937ec628c5f27228fe1c862
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2024-02-05 06:25:48 +0530

    Create generateBinaryStrings.java

---
+++ Back Tracking/generateBinaryStrings.java
@@ -0,0 +1,27 @@
+//generate all possible binary strings of n bit
+//backTracking
+package DSA;
+
+public class generateBinaryStrings {
+	public static void display(int[] arr) {
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+	public static void BinaryStrings(int[] arr,int n) {
+		if(n<1) {
+			display(arr);
+		}else {
+			arr[n-1]=0;
+			BinaryStrings(arr,n-1);
+			arr[n-1]=1;
+			BinaryStrings(arr,n-1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] arr=new int[5];
+		BinaryStrings(arr,5);
+	}
+}
